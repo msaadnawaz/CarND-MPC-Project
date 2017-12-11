44723d3b0fab44f2499a4bc28461b8d74f8447dd
Muhammad Saad Nawaz <saadnawaz27@yahoo.com>
2017-12-11 15:33:39 +0100

    actuation for t>0

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -111,10 +111,10 @@ public:
 			// Only consider the actuation at time t.
 			AD<double> delta0 = vars[delta_start + t];
 			AD<double> a0 = vars[a_start + t];
-//			if(t>0){
-//				a0 = vars[a_start + t - 1];
-//				delta0 = vars[delta_start + t - 1];
-//			}
+			if(t>0){
+				a0 = vars[a_start + t - 1];
+				delta0 = vars[delta_start + t - 1];
+			}
 
 			AD<double> f0 = coeffs[0] + coeffs[1] * x0 + coeffs[2] * x0 * x0 + coeffs[3] * x0 * x0 * x0;
 			AD<double> psides0 = CppAD::atan(coeffs[1] + 2* coeffs[2] * x0 + 3 * coeffs[3] * x0 * x0);
